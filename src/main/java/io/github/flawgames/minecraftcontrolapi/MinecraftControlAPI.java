076dc3705f8ea3187224be902236e26f34e66daa
MinecraftUserHAN1110 <93987816+MinecraftUserHAN1110@users.noreply.github.com>
2024-04-06 21:05:11 +0900

    Initial commit

---
+++ src/main/java/io/github/flawgames/minecraftcontrolapi/MinecraftControlAPI.java
@@ -0,0 +1,17 @@
+package io.github.flawgames.minecraftcontrolapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftControlAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
